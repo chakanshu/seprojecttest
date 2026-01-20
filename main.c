8a9ec5e4e034e74406bce9c3a0d615f074c2a33a
chakanshu <stevelegoman55@gmail.com>
2026-01-20 16:48:58 +0530

    modular tabular errored

--- main.c
+++ main.c
@@ -4,14 +4,16 @@ Structures to represent student data
 Grading based on percentage
 display percentage and cgpa
 display in tabular format
+modular programming (20/01/2026 16:47)
 
 To Do
 Class average percentage
 Highest and lowest percentage 
 number of students in each category
-
 error handling
-modular programming
+
+Errors
+it shows pass for everyone
 */
 
 #include <stdio.h>
@@ -29,18 +31,15 @@ int main(int argc, char *argv[]){
 
     struct student student[100];
     int n = readStudents(student, filename);
-
-    printf("");
-    printf("\nID   m1 M1 m2 M2 m3 M3 m4 M4 m5 M5 G1 G2 G3 G4 G5 P/F  %%     CGPA\n\n");
-
+    printf("+---------------------------------------------------------------------+");
+    printf("\n| ID   | m1 M1 m2 M2 m3 M3 m4 M4 m5 M5 G1 G2 G3 G4 G5 P/F  %%     CGPA |\n");
+    printf("+---------------------------------------------------------------------+\n");
     for(int i = 0; i < n; i++){
         int totalMarks = 0;
-        int failFlag = 0;
         int cgpaSum = 0;
+        int failFlag = 0;
 
-        printf("%s ", student[i].id);
-        // Original line to print names 
-        // printf("%s %s ", student[i].id, student[i].name);
+        printf("| %s | ", student[i].id);
 
         for(int j = 0; j < 5; j++){
             printf("%d %d ",
@@ -53,30 +52,33 @@ int main(int argc, char *argv[]){
                 student[i].subjects[j].minor +
                 student[i].subjects[j].major;
 
-            char grade[3];
-            GradeFunction(subjectTotal, grade);
+            GradeFunction(subjectTotal, student[i].subjects[j].grade);
 
-            if(strcmp(grade, "F ") == 0){
+            if(strcmp(student[i].subjects[j].grade, "F") == 0){
                 failFlag = 1;
             }
 
-            cgpaSum += GradePoint(grade);
+            cgpaSum += GradePoint(student[i].subjects[j].grade);
             totalMarks += subjectTotal;
 
-            printf("%s ", grade);
+            printf("%s ", student[i].subjects[j].grade);
         }
 
-        float percentage = totalMarks / 5.0;
-        float cgpa = cgpaSum / 5.0;
+        student[i].percentage = totalMarks / 5.0;
+        student[i].cgpa = cgpaSum / 5.0;
 
         if(failFlag){
-            printf("FAIL ");
+            strcpy(student[i].status, "FAIL");
         } else {
-            printf("PASS ");
+            strcpy(student[i].status, "PASS");
         }
 
-        printf("%.2f %.2f\n", percentage, cgpa);
+        printf("%s %.2f %.2f |\n",
+               student[i].status,
+               student[i].percentage,
+               student[i].cgpa);
     }
 
+    printf("+---------------------------------------------------------------------+");
     return 0;
 }
--- studentstructure.h
+++ studentstructure.h
@@ -4,12 +4,16 @@
 struct subject {
     int minor;
     int major;
+    char grade[5];
 };
 
 struct student {
     char id[20];
     char name[50];
     struct subject subjects[5];
+    char status[3]; // PASS/FAIL
+    float percentage;
+    float cgpa;
 };
 
 #endif
